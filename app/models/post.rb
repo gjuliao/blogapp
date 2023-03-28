3626e11d27aa26702c2b4321da1e9ef51f5c1048
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-28 12:53:57 -0500

    Added update counter for likes, posts and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_comment_count
+    self.comments_count = comments.count
+    save
+  end
+
+  def update_likes_count
+    self.likes_count = likes.count
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
+
+  def update_posts_count
+    self.posts_count = posts.count
+    save
+  end
 end
