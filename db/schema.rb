8b280452a92eac8ccca0fe04e15dda68a2593a18
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-29 15:49:14 -0500

    Added user_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/BlockLength, Style/NumericLiterals, Style/StringLiterals
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_194717) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_204048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_194717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.integer "user_id"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
