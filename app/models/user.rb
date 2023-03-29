d4030d62dfdbed6d8c9e0b37117f34c666d47d2b
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-29 15:08:39 -0500

    In User class added recent_method and update_posts_count

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  def update_posts_count
+    self.posts_counter = posts.count
+    save
+  end
 end
