ecea9d28e54a197e74920dc3cc8a1628d8ee37d4
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-29 15:57:28 -0500

    Added update_likes_count method to likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
+
+  def update_likes_count
+    post.likes_counter = likes.count
+    save
+  end
 end
