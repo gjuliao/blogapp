9b688f2bd10f28bf303dbac4866835550cb619e9
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-29 17:57:49 -0500

    In Like class added class_name in belongs_to

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  belongs_to :post
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
   def update_likes_count
     post.likes_counter = likes.count
