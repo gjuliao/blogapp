e6feef99aba5d52a6f7e6e43309ce6bda2e53c90
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-30 15:54:14 -0500

    Added post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+    # Validations
+    validates :title, presence: true, length: { maximum: 250 }, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
+    validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
