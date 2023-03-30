46a80514263fec89369624ba60ae6ba83f0d34b8
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-30 15:52:33 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  # Validations
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
