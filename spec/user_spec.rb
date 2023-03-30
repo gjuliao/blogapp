ddea38279686e029a1830221eaa249f815179c47
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-30 16:10:10 -0500

    User tests 4 passing

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,14 +1,26 @@
 require_relative 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject do
-    User.new(name: 'John', photo: 'asdfasfd', bio: 'adfasfasdfasdf', posts_counter: 0)
-  end
+  subject { User.new(name: 'John', photo: 'asdfasfd', bio: 'adfasfasdfasdf', posts_counter: 0) }
 
   before { subject.save }
 
-  it 'title should be present' do
+  it 'No name should be invalid' do
     subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'Name length should be less than 50' do
+    subject.name = 'b' * 55
+    expect(subject).to_not be_valid
+  end
+
+  it 'Posts counter should be greater than or equal to 0' do
+    subject.posts_counter = -1
     expect(subject).to_not be_valid
   end
 end
