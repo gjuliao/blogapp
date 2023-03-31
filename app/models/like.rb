50be08a9280da03e13e4081e3b53a0d5554266fd
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-31 15:04:28 -0500

    Made method adjustments

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
-  def update_likes_count
-    post.likes_counter = likes.count
+  def update_post_likes_count
+    post.likes_counter = post.likes.count
     save
   end
 end
