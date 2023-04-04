acf7e4e0b5ebff4bfd15cb05535353d28a4dc757
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-04 15:07:56 -0500

    Added the Posts controller test to index and show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    subject { get '/users/:id/posts' }
+    before { subject }
+
     it 'returns http success' do
       get '/users/:user_id/posts'
       expect(response).to have_http_status(:success)
     end
+
+    it 'should render the index template' do
+      expect(response).to render_template(:index)
+    end
   end
 
   describe 'GET /show' do
+    subject { get '/users/:id/posts/:id' }
+    before { subject }
+
     it 'returns http success' do
       get '/users/:user_id/posts/:id'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render the show template' do
+      expect(response).to render_template(:show)
     end
   end
 end
