e1ff323dd263899ffd745e532e6866ebbe3ba2d5
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-04 15:05:58 -0500

    Added test for user index and show

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -2,9 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    subject { get '/users' }
+    before { subject }
+
     it 'returns http success' do
       get '/users'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render the index page' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'body includes Here could be all the users' do
+      get '/users'
+      expect(response.body).to include('Here could be all the users')
+    end
+  end
+
+  describe 'GET /show' do
+    subject { get '/users/:id' }
+    before { subject }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should render the show page' do
+      expect(response).to render_template(:show)
     end
   end
 end
