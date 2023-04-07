2984bd4b85c9cbac2b8a11bbdcb3e57123b946f1
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-07 10:07:49 -0500

    Added the like create method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,22 @@
 class LikesController < ApplicationController
   def create
+    @like = Like.new(like_params)
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @like.author = ApplicationController.current_user
+    @like.post = @user.posts.find(params['post_id'])
+    if @like.save
+      flash[:success] = 'Like created successfully'
+      redirect_to user_posts_path(ApplicationController.current_user, @post)
+    else
+      flash[:error] = 'Error: Like could not be saved'
+      render 'new'
+    end
+  end
+
+  private
+
+  def like_params
+    params.require(:like)
   end
 end
