6619761c5ea20c88078b37297f54d7e96bece75d
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-12 11:21:56 -0500

    Added email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,18 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.office365.com',
+    port:                 587,
+    user_name:            'gio_microverse@outlook.com',
+    password:             'Microverse1!',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
