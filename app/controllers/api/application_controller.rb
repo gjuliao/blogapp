93c919c2b0a705c9c7b1d2344a679af327bee7cb
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-13 12:46:33 -0500

    Fixed linters

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,11 +1,10 @@
 class Api::ApplicationController < ActionController::API
-    #before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
   end
-  
+end
