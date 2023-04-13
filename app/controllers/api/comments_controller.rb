d7388d5ebc512ee47b7a2327ecfa450e72989c5a
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-13 11:41:24 -0500

    Added in api comment controller the create method

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -18,23 +18,10 @@ class Api::CommentsController < ApplicationController
       @comment.post = @user.posts.find(params['post_id'])
   
       if @comment.save
-        flash[:success] = 'Comment created successfully'
-        redirect_to user_posts_path(current_user, @post)
+        render json: { status: :created, data: @comment }
       else
-        flash[:error] = 'Error: Comment could not be saved'
-        render 'new'
+        render json: { error: @comment.errors, status: :unprocessable_entity }
       end
-    end
-  
-    def destroy
-      @user = User.find(params[:user_id])
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.find(params[:id])
-      authorize! :destroy, @comment
-      @post.comments_counter -= 1
-      @comment.destroy
-      @post.save
-      redirect_to user_post_path(@user, @post)
     end
   
     private
