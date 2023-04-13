902cb1cbb1d888b3d2dfe7a2b0164989d2cc3e91
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-13 11:27:59 -0500

    Added api controllers

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+    def new
+      @post = Post.find(params[:post_id])
+      @comment = Comment.new
+    end
+  
+    def create
+      @user = User.find(params[:user_id])
+      @post = Post.find(params[:post_id])
+      @comment = Comment.new(comment_params)
+      @comment.author = current_user
+      @comment.post = @user.posts.find(params['post_id'])
+  
+      if @comment.save
+        flash[:success] = 'Comment created successfully'
+        redirect_to user_posts_path(current_user, @post)
+      else
+        flash[:error] = 'Error: Comment could not be saved'
+        render 'new'
+      end
+    end
+  
+    def destroy
+      @user = User.find(params[:user_id])
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+      authorize! :destroy, @comment
+      @post.comments_counter -= 1
+      @comment.destroy
+      @post.save
+      redirect_to user_post_path(@user, @post)
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
+  
