30bf6f96a050171bab9afd7ededa8a69e078965e
PrangonGhose <prangonghose@gmail.com>
2023-04-14 01:07:49 +0600

    Practice DRY to remove unnecessary methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -11,39 +11,6 @@ class Api::PostsController < Api::ApplicationController
     end
   end
 
-  def show
-    @user = current_user
-    @post = Post.find(params[:id])
-    @comments = @post.comments
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.author = current_user
-    @post.comments_counter = 0
-    @post.likes_counter = 0
-    if @post.save
-      flash[:success] = 'Post created successfully'
-      redirect_to user_posts_path(current_user)
-    else
-      flash[:error] = 'Error: Post could not be saved'
-      render 'new'
-    end
-  end
-
-  def destroy
-    @user = current_user
-    @post = Post.find(params[:id])
-    authorize! :destroy, @post
-    @user.posts_counter -= 1
-    @post.destroy
-    redirect_to user_posts_path(@user)
-  end
-
   private
 
   def post_params
