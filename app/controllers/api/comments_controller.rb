c53e9dbc318c77cf4a0cdfed76326715a34acf9a
PrangonGhose <prangonghose@gmail.com>
2023-04-14 01:00:32 +0600

    Fix linters

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,37 +1,35 @@
 class Api::CommentsController < Api::ApplicationController
-
-    def index
-      @post = Post.find(params[:post_id])
-      if @post
-        render json: { status: 'ok', data: @post.comments }
-      else
-        render json: { status: 'Error', error: 'Post Not Found' }
-      end
-    end
-
-    def new
-      @post = Post.find(params[:post_id])
-      @comment = Comment.new
-    end
-  
-    def create
-      @user = User.find(params[:user_id])
-      @post = Post.find(params[:post_id])
-      @comment = Comment.new(comment_params)
-      @comment.author = current_user
-      @comment.post = @user.posts.find(params['post_id'])
-  
-      if @comment.save
-        render json: { status: :created, data: @comment }
-      else
-        render json: { error: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text)
+  def index
+    @post = Post.find(params[:post_id])
+    if @post
+      render json: { status: 'ok', data: @post.comments }
+    else
+      render json: { status: 'Error', error: 'Post Not Found' }
     end
   end
-  
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+    @comment.post = @user.posts.find(params['post_id'])
+
+    if @comment.save
+      render json: { status: :created, data: @comment }
+    else
+      render json: { error: @comment.errors, status: :unprocessable_entity }
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,53 +1,52 @@
 class Api::PostsController < Api::ApplicationController
-    load_and_authorize_resource
-  
-    def index
-      @user = User.find(params[:user_id])
-      @posts = @user.posts.includes(:comments, :likes)
-      if @user
-        render json: @posts
-      else
-        render json: { status: 'Error', error: 'User Not Found' }
-      end
-    end
-  
-    def show
-      @user = current_user
-      @post = Post.find(params[:id])
-      @comments = @post.comments
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @post = Post.new(post_params)
-      @post.author = current_user
-      @post.comments_counter = 0
-      @post.likes_counter = 0
-      if @post.save
-        flash[:success] = 'Post created successfully'
-        redirect_to user_posts_path(current_user)
-      else
-        flash[:error] = 'Error: Post could not be saved'
-        render 'new'
-      end
-    end
-  
-    def destroy
-      @user = current_user
-      @post = Post.find(params[:id])
-      authorize! :destroy, @post
-      @user.posts_counter -= 1
-      @post.destroy
-      redirect_to user_posts_path(@user)
-    end
-  
-    private
-  
-    def post_params
-      params.require(:post).permit(:title, :text)
+  load_and_authorize_resource
+
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments, :likes)
+    if @user
+      render json: @posts
+    else
+      render json: { status: 'Error', error: 'User Not Found' }
     end
   end
-  
+
+  def show
+    @user = current_user
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      flash[:success] = 'Post created successfully'
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:error] = 'Error: Post could not be saved'
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @post = Post.find(params[:id])
+    authorize! :destroy, @post
+    @user.posts_counter -= 1
+    @post.destroy
+    redirect_to user_posts_path(@user)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
