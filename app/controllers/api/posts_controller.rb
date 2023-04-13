c52284888e027dc02b42a4ac0d552eb523135904
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-13 17:49:21 -0500

    Edited api posts index and show methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,49 +1,19 @@
 class Api::PostsController < Api::ApplicationController
-  load_and_authorize_resource
 
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments, :likes)
-    if @user
+    @posts = Post.where(author_id: params[:user_id])
+    if @posts
       render json: @posts
     else
-      render json: { status: 'Error', error: 'User Not Found' }
+      render json: { status: 'Error', error: 'User has no posts' }
     end
   end
 
   def show
-    @user = current_user
-    @post = Post.find(params[:id])
-    @comments = @post.comments
+    @post = Post.where(id: params[:id])
+    render json: @post
   end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.author = current_user
-    @post.comments_counter = 0
-    @post.likes_counter = 0
-    if @post.save
-      flash[:success] = 'Post created successfully'
-      redirect_to user_posts_path(current_user)
-    else
-      flash[:error] = 'Error: Post could not be saved'
-      render 'new'
-    end
-  end
-
-  def destroy
-    @user = current_user
-    @post = Post.find(params[:id])
-    authorize! :destroy, @post
-    @user.posts_counter -= 1
-    @post.destroy
-    redirect_to user_posts_path(@user)
-  end
-
+  
   private
 
   def post_params
