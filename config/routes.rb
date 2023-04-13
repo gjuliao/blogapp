ccf9148bb9bde835f263a00cae41e006b5d42167
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-13 11:23:31 -0500

    Added api routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,14 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts/:id", to: "posts#show"
   post "/users/:user_id/posts/new", to: "posts#create"
   delete '/users/:user_id/posts/:id', to: 'posts#destroy'
+
+    #API routes
+    namespace :api do
+      post 'login', to: 'login#index'
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:create, :index]
+        end
+      end
+    end
 end
